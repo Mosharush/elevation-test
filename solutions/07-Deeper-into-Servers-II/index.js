8cb7c4a88014950936623c5b7c7ef5adfa3bda1c
Moshe Harush <mosheh@yad2.co.il>
2020-02-25 12:50:59 +0200

    07 - Deeper into Servers II - done

---
+++ solutions/07-Deeper-into-Servers-II/index.js
@@ -0,0 +1,38 @@
+// Deeper into Servers II
+//---------------------﻿
+
+// Exercise 2
+const { app, get } = require('@eac/webserver')
+const accessManager = require('@eac/access-restricted')
+const axios = require('axios');
+
+const port = 1337
+const limitAccessCount = 5
+const apiUrl = 'https://recipes-goodness.herokuapp.com/recipes/%s'
+
+let globalCounter = 0
+
+const checkLimitAccess = ({ res, next }) => {
+    globalCounter++;
+    const allowAccess = accessManager.checkAccess( globalCounter, limitAccessCount )
+
+    if ( ! allowAccess ) {
+        res.json({
+            error: "Too many requests",
+            counter: globalCounter
+        });
+        return res.end()
+    }
+    next()
+}
+
+app.use('/recipe/:ingredient', checkLimitAccess, async ( req, res ) => {
+    const apiCurrent = apiUrl.replace('%s', req.params.ingredient)
+    const recipeData = await axios.get( apiCurrent );
+    const recipes = recipeData.data.results;
+    const response = recipes.length !== 0 ? recipes.splice(0, 3) : { empty: true };
+
+    res.json( response );
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
